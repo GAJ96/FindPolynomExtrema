6c6fe1643dc6a855576246314f5e347af8c02f6b
Gabriel Juutinen <73961723+GAJ96@users.noreply.github.com>
2021-03-07 08:05:14 +0100

    Fixed some bugs
    Changed so the correct interval will be returned. Changed so the correct guess re-enters/can enter the inner while loop. Added timer. Corrected the min/max checker (does still not handle inflexion points correctly).

--- FindPolynomExtrema.py
+++ FindPolynomExtrema.py
@@ -1,6 +1,8 @@
 
 import numbers
 import numpy
+from time import process_time
+
 
 # Inputs
 print("In order to solve for the polinomials exreme points it needs to be represented as below:")
@@ -11,13 +13,16 @@ guess = float(input("Make a guess on where the extrema might be located on the x
 tol = float(input("Maximum amount of tolerated error: "))
 step = float(input("How large steps shall we begin the search process with? Enter: "))
 
+# Start timer
+t0 = process_time()
+
 # First derivative
 def fprim(x):
     return(a1*b*(x**(b-1)))
 
 #The second derivative is neede to determine if the exterma is a min or max
 def fbis(x):
-    return(a*b*(b-1)*(x**(b-2)))
+    return(a1*b*(b-1)*(x**(b-2)))
 
 # The bounds of the extrema
 lowlim = 0
@@ -37,38 +42,44 @@ typ = " "
 if fprim(guess) > 0:
 
     while step > tol:
+        guesspos -= 2*step
+        guessneg += 2*step
         while fprim(guesspos)  > 0 and fprim(guessneg) > 0:
             guesspos += step
             guessneg -= step
             iterations += 1
         step = step/2
+    step = 2*step
 
     if fprim(guesspos) < 0:
-        lowlim = guesspos
-        uplim = guesspos + step
+        lowlim = guesspos - step
+        uplim = guesspos
 
     elif fprim(guessneg) < 0:
-        lowlim = guessneg - step
-        uplim = guessneg
+        lowlim = guessneg
+        uplim = guessneg + step
 
 
 # If slope negative , do this:
 if fprim(guess) < 0:
 
     while step > tol:
+        guesspos -= 2*step
+        guessneg += 2*step
         while fprim(guesspos) < 0 and fprim(guessneg) < 0:
             guesspos += step
             guessneg -= step
             iterations += 1
         step = step/2
+    step = 2*step
 
     if fprim(guesspos) > 0:
-        lowlim = guesspos
-        uplim = guesspos + step
+        lowlim = guesspos - step
+        uplim = guesspos
 
     elif fprim(guessneg) > 0:
-        lowlim = guessneg - step
-        uplim = guessneg
+        lowlim = guessneg
+        uplim = guessneg + step
 
 # Checking if its a min or a max
 if fbis( (lowlim + uplim)/2 ) < 0:
@@ -76,11 +87,14 @@ if fbis( (lowlim + uplim)/2 ) < 0:
 elif fbis( (lowlim + uplim)/2 ) > 0:
     typ = "minimum"
 
-print()
-print()
-print()
+print("")
+print("")
+print("")
+print("")
 
 # Output:
 print("The extrema is predicted to be in the boudns of (",lowlim,";",uplim,").")
 print("The extrema is a ",typ,".")
 print("It took ",iterations, "iterations to find this",typ,".")
+t1 = process_time()
+print("Timer: [",t1-t0,"]")
